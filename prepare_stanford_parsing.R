43402b9aabd5a407b8cf0c68c6bd05a9e7c95600
ningcui1 <ningcui1@hotmail.com>
2015-06-02 13:19:23 +0800

    add prepare_stanford_parsing.R

---
+++ prepare_stanford_parsing.R
@@ -0,0 +1,19 @@
+# clean annotation and save to file for parser
+library(data.table)
+
+Sys.setlocale(locale="Chinese") 
+sFrame <- as.data.frame(fread(input = "ClassifierDataBase/preprocessing/sFrame_encoding.csv",stringsAsFactors = F))
+#sFrame <- read.csv("ClassifierBase/CCD/data/sFrame.csv",fileEncoding="UTF-8",stringsAsFactors=FALSE,nrows = 200)
+Encoding(sFrame[,1])<-"UTF-8"
+Encoding(sFrame[,2])<-"UTF-8"
+Encoding(sFrame[,3])<-"UTF-8"
+Encoding(sFrame[,4])<-"UTF-8"
+
+del.anno.keep.space <- function(sentences){
+    gsub("(.*?)/..?.? ","\\1 ",sentences)
+}
+
+sentences <- del.anno.keep.space(sFrame$sentence)
+
+path <- "ClassifierDataBase/preprocessing/classifiers-toParse.txt"
+write.table(sentences,path,fileEncoding="UTF-8",quote = F,row.names = F,col.names = F,eol = "\n")
